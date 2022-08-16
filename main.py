b8ee87de47d1e18315ca826d7c6722a16c238412
hoangks5 <hoangnd.ute@gmail.com>
2022-08-16 16:32:41 +0700

    --- FastAPI ---

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ from fastapi import FastAPI, File, UploadFile #import class FastAPI() từ thư 
 import requests
 app = FastAPI() # gọi constructor và gán vào biến app
 
-@app.get("/") # giống flask, khai báo phương thức get và url
+@app.get("/min") # giống flask, khai báo phương thức get và url
 async def root(): # do dùng ASGI nên ở đây thêm async, nếu bên thứ 3 không hỗ trợ thì bỏ async đi
     return {"message": "Xin chào đây là đội Xanh nước biển"}
