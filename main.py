0a85c95fd3d0655ddc356f89c604c8c64f3bec30
hoangks5 <hoangnd.ute@gmail.com>
2022-08-17 09:56:03 +0700

    --- FastAPI ---

--- main.py
+++ main.py
@@ -8,4 +8,7 @@ async def price_main(token): # do dùng ASGI nên ở đây thêm async, nếu b
 
 @app.get("/{token}") # giống flask, khai báo phương thức get và url
 async def price1(token): # do dùng ASGI nên ở đây thêm async, nếu bên thứ 3 không hỗ trợ thì bỏ async đi
-    return API.test(token)
+    
+    
+    
+    return False
