f50c6fcc66282599b92f0e5e90b2c2b86ec30029
hoangks5 <hoangnd.ute@gmail.com>
2022-08-17 15:53:42 +0700

    --- FastAPI ---

--- show.py
+++ show.py
@@ -14,6 +14,19 @@ def get_price_min(token):
         'timestamp' : timest,
         'price' : response,
     }
+    return avg
+
+def get_price_max(token):
+    token = token.split('-')[0]
+    url = "https://pricefeedfastapi.herokuapp.com/max/"+token
+    response = requests.get(url).json()['price']
+    timest = time.time()
+    avg = {
+        'source' : 'Max',
+        'token' : token,
+        'timestamp' : timest,
+        'price' : response,
+    }
 data = []
 plt.bar('hi',10)
 plt.show()
