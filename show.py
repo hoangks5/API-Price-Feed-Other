e7bc1bda665befce2a6cf858ed53f11ef536897d
hoangks5 <hoangnd.ute@gmail.com>
2022-08-17 15:53:02 +0700

    --- FastAPI ---

--- show.py
+++ show.py
@@ -1,6 +1,18 @@
 import matplotlib.pyplot as plt
 import save_price
+import time
 
+def get_price_min(token):
+    token = token.split('-')[0]
+    url = "https://pricefeedfastapi.herokuapp.com/min/"+token
+    response = requests.get(url).json()['price']
+    timest = time.time()
+    avg = {
+        'source' : 'Min',
+        'token' : token,
+        'timestamp' : timest,
+        'price' : response,
+    }
 data = []
 plt.bar('hi',10)
 plt.show()
