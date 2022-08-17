29f129aa20f43bbadfa10f1415f91e54369dae9e
hoangks5 <hoangnd.ute@gmail.com>
2022-08-17 09:48:23 +0700

    --- FastAPI ---

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI #import class FastAPI() từ thư viện fastapi
 import requests
+import API
 app = FastAPI() # gọi constructor và gán vào biến app
 @app.get("/min/{token}") # giống flask, khai báo phương thức get và url
 async def price_min(token): # do dùng ASGI nên ở đây thêm async, nếu bên thứ 3 không hỗ trợ thì bỏ async đi
