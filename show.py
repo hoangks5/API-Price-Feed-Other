9dbf6a28506cb464d17ad01aee49548ad16626bf
hoangks5 <hoangnd.ute@gmail.com>
2022-08-23 11:59:39 +0700

    --- FastAPI ---

--- show.py
+++ show.py
@@ -5,6 +5,7 @@ import  threading
 import pymongo
 import certifi
 
+TOKENS = ['BTC-USD', 'ETH-USD', 'BNB-USD', 'DOGE-USD', 'LINK-USD', 'UNI-USD', 'SOL-USD', 'MATIC-USD', 'LUNA-USD', 'DOT-USD', 'ATOM-USD']
 
 myclient = pymongo.MongoClient("mongodb+srv://hoangks5:YrfvDz4Mt8xrrHxi@cluster0.tcbxc.mongodb.net/",tlsCAFile=certifi.where())
 mydb = myclient['compare']
