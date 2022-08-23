8fa7109083bac3cba7169e4a06b7e18364247cfd
hoangks5 <hoangnd.ute@gmail.com>
2022-08-23 10:46:17 +0700

    --- FastAPI ---

--- show.py
+++ show.py
@@ -3,4 +3,9 @@ import save_price
 import time
 import requests
 import  threading
+import pymongo
 
+client = pymongo.MongoClient("mongodb+srv://hoangks5:YrfvDz4Mt8xrrHxi@cluster0.tcbxc.mongodb.net/",tlsCAFile=certifi.where())
+mydb = client['compare']
+mycol = mydb['data']
+TOKENS = ['BTC-USD', 'ETH-USD', 'DOGE-USD', 'LINK-USD',  'SOL-USD', 'MATIC-USD',  'DOT-USD', 'ATOM-USD']
