ef60f18eb16bf3d191207483ea1e8a76d40c4be6
hoangks5 <hoangnd.ute@gmail.com>
2022-08-23 10:05:26 +0700

    --- FastAPI ---

--- main.py
+++ main.py
@@ -9,17 +9,17 @@ async def price_main(token): # do dùng ASGI nên ở đây thêm async, nếu b
     return API.price_main(token)
 
 @app.get("/min/{token}") # giống flask, khai báo phương thức get và url
-async def price1(token): # do dùng ASGI nên ở đây thêm async, nếu bên thứ 3 không hỗ trợ thì bỏ async đi
+async def price_1(token): # do dùng ASGI nên ở đây thêm async, nếu bên thứ 3 không hỗ trợ thì bỏ async đi
     return API.price_min(token)
 
 
 @app.get("/max/{token}") # giống flask, khai báo phương thức get và url
-async def price2(token): # do dùng ASGI nên ở đây thêm async, nếu bên thứ 3 không hỗ trợ thì bỏ async đi
+async def price_2(token): # do dùng ASGI nên ở đây thêm async, nếu bên thứ 3 không hỗ trợ thì bỏ async đi
     return API.price_max(token)
 
 
 @app.get("/noise/{token}")
-async def noise(token):
+async def price_3(token):
     token = (token + '-usd').upper()
     price = API.get_binance_price(token)['price'] 
     return {'price' : price + price*random.randint(1,100)*0.001}
