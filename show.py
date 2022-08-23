5ad865d47fb2659becab05b50792061e9f3e3c93
hoangks5 <hoangnd.ute@gmail.com>
2022-08-23 10:58:57 +0700

    --- FastAPI ---

--- show.py
+++ show.py
@@ -7,14 +7,6 @@ import pymongo
 import certifi
 
 
-
-client = pymongo.MongoClient("mongodb+srv://hoangks5:YrfvDz4Mt8xrrHxi@cluster0.tcbxc.mongodb.net/",tlsCAFile=certifi.where())
-mydb = client['compare']
-mycol = mydb['data']
-TOKENS = ['BTC-USD', 'ETH-USD', 'DOGE-USD', 'LINK-USD',  'SOL-USD', 'MATIC-USD',  'DOT-USD', 'ATOM-USD']
-
-def show(token):
-    token1 = token.split('-')[0]
-    datas = mycol.find({})
-    print(datas)
-show('BTC-USD')
+myclient = pymongo.MongoClient("mongodb+srv://hoangks5:YrfvDz4Mt8xrrHxi@cluster0.tcbxc.mongodb.net/",tlsCAFile=certifi.where())
+mydb = myclient['price']
+mycol = mydb['datanew']
