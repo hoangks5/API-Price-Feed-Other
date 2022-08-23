055500f7bb40cef37d2344827b0a03c919ad9066
hoangks5 <hoangnd.ute@gmail.com>
2022-08-23 10:59:27 +0700

    --- FastAPI ---

--- show.py
+++ show.py
@@ -10,3 +10,7 @@ import certifi
 myclient = pymongo.MongoClient("mongodb+srv://hoangks5:YrfvDz4Mt8xrrHxi@cluster0.tcbxc.mongodb.net/",tlsCAFile=certifi.where())
 mydb = myclient['price']
 mycol = mydb['datanew']
+
+datas = mycol.find({})
+for data in datas:
+    print(data)
