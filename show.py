57c4c45de5a9dfd1a9ad0562459c4caf17ed8eb7
hoangks5 <hoangnd.ute@gmail.com>
2022-08-23 10:52:27 +0700

    --- FastAPI ---

--- show.py
+++ show.py
@@ -14,7 +14,7 @@ mycol = mydb['data']
 TOKENS = ['BTC-USD', 'ETH-USD', 'DOGE-USD', 'LINK-USD',  'SOL-USD', 'MATIC-USD',  'DOT-USD', 'ATOM-USD']
 
 def show(token):
-    token = token.split('-')[0]
-    datas = mycol.find({token:token})
+    token1 = token.split('-')[0]
+    datas = mycol.find({token:token1})
     print(datas)
 show('BTC-USD')
